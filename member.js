026ca9a958f9fd8054d46e06f1ad5e5ff1b33a40
Marcelo <web@croop.cl>
2023-10-30 19:36:19 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,13 @@
+function skillsMember() {
+    var skills = ['HTML', 'CSS', 'JS', 'React', 'Node'];
+    var member = {
+        name: 'John',
+        age: 25,
+        skills: skills,
+        // skills: skillsMember,
+        // skills: function() {
+        //     return skills;
+        // }
+    };
+    return member;
+} 
